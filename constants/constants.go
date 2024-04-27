97c1de7c9c31e0f11f093d21a80d8bf0ef44f68d
MAREKOR555 <MAREKOR555@INTERIA.PL>
2024-04-27 18:11:43 +0200

    added types to all constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,8 +1,8 @@
 package constants
 
 const (
-	WINDOW_WIDTH          = 800
-	WINDOW_HEIGHT         = 450
+	WINDOW_WIDTH  int32   = 800
+	WINDOW_HEIGHT int32   = 450
 	BLOCK_WIDTH   float32 = 100
 	BLOCK_HEIGHT  float32 = 60
 )
